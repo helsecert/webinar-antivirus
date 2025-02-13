06a3af0520f9f661003adff1037734a9c85aa205
Marius Myhre <marius.ibenfeldt.myhre@nhn.no>
2025-02-13 14:41:42 +0100

    Create send2graylog.ps1

---
+++ send2graylog.ps1
@@ -0,0 +1,18 @@
+# IP adresse eller hostnavn til Graylog-serveren
+$graylogserver = "127.0.0.1"
+
+# Porten til GELF TCP-inputten
+$port = "12201"
+
+# Hvor lenge tilbake scriptet skal hente logger fra. Om du kjører scriptet hvert minutt, la den være på -70 sekunder for å ha litt overlapp
+$starttime = (Get-Date).AddSeconds(-70)
+
+$events = Get-WinEvent -FilterHashtable @{
+    LogName="Microsoft-Windows-Windows Defender/Operational"; # Se i Defender-loggene
+    Id=@(1006, 1015, 1116, 5001, 5013); # Ta med de viktigste eventene
+    StartTime=$starttime
+} -ErrorAction SilentlyContinue
+
+if ($events) {
+    $events | Send-PSGelfTCPFromObject -GelfServer $graylogserver -Port $port
+}
